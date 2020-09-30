aa06c20f955823fcdd56dfce81a365cdae6e7459
Martin <liman.martinn@gmail.com>
2020-09-30 14:38:30 +0700

    adding email

---
+++ Test Suites/Custom footer test.ts
@@ -0,0 +1,104 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Custom footer test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>e5a2a3e4-7018-418c-87f4-5033b3b11c80</testSuiteGuid>
+   <testCaseLink>
+      <guid>8e6fe22e-4eae-47f8-99fe-c449f293b91e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/Custom Footer</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>1249d0c8-39d9-4aa9-b4c0-cc554e42d930</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e744f87c-af6a-47c6-be4d-7d13733c26c8</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9b1c69a1-e8eb-4590-be4b-39d79f6922eb</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>4352cda1-2c52-49c3-a6fe-1dd8c9953ffd</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f4b8957f-7e18-4f40-a8ce-1a72e648525f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_005</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>0c947435-fdba-4f82-9995-cb23139a8bf3</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_006</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>e1a202d2-9b63-47c7-af41-349854c81045</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_007</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>86791b26-2806-486e-993d-2ca36279da2d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_008</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>bad5b179-8c79-44d6-b8c7-f36aecf06dfe</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_009</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>9d2a7385-cc90-42d4-b977-3b6035a468e6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_010</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>dd8906bb-cfb7-4470-a0c2-760148fcec6f</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_011</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>22b43934-ef70-44ce-8884-8b91bdaea9a5</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_012</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>13c48207-9da9-4130-8aee-2eda22249e9e</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_013</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>46770dd0-2ea8-4306-ab46-3f55e3150f7b</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Custom Footer Test Case/TEST CASE/TC_FOOTER_UI_014</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
---
+++ Test Suites/Homepage category test.ts
@@ -0,0 +1,56 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Homepage category test</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>21fe82d7-4ed4-40ab-80da-153c3f34f26c</testSuiteGuid>
+   <testCaseLink>
+      <guid>cb00ac41-7c5e-4356-a76f-3b3d48fa5c69</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Extra Home Category Test Case/TEST CASE/COVID19/TC_CAT_UI_001</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fe711ce8-b260-4ca5-8ff3-fe07c5878e71</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Extra Home Category Test Case/TEST CASE/COVID19/TC_CAT_UI_002</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>f5cdc551-dfe2-437b-b88a-cc3d01622810</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Extra Home Category Test Case/TEST CASE/COVID19/TC_CAT_UI_003</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>ababf6cc-a2b4-4979-89a8-33b0f88d9e53</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Extra Home Category Test Case/TEST CASE/COVID19/TC_CAT_UI_004</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>299c482c-5cc4-4501-ac72-6a04737a356d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Extra Home Category Test Case/TEST CASE/COVID19/TC_CAT_UI_005</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>fcd9bc1c-7339-40b6-aaa3-4a1a5c68c0d6</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Extra Home Category Test Case/TEST CASE/COVID19/TC_CAT_UI_006</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>525d07cb-0d4f-4dfc-9e5f-c5ae117fe16a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Extra Home Category Test Case/TEST CASE/COVID19/TC_CAT_UI_007</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
