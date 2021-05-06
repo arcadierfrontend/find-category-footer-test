6a5bcf38e8eb7ea819ecb9f2bf4ec6ef8ef4b58a
arcadierfrontend <71870637+arcadierfrontend@users.noreply.github.com>
2021-05-06 14:59:30 +0800

    Updated url to staging
    Updated url to staging

--- Libs/TempTestCase1617690563338.groovy
+++ Libs/TempTestCase1617690563338.groovy
@@ -18,9 +18,9 @@ DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.w
 DriverCleanerCollector.getInstance().addDriverCleaner(new com.kms.katalon.core.testng.keyword.internal.TestNGDriverCleaner())
 
 
-RunConfiguration.setExecutionSettingFile('C:\\Users\\Lexoter\\AppData\\Local\\Temp\\Katalon\\Test Cases\\Extra Home Category Test Case\\TEST CASE\\GDA\\TC_CAT_UI_004\\20210406_135348\\execution.properties')
+RunConfiguration.setExecutionSettingFile('C:\\Users\\Lexoter\\AppData\\Local\\Temp\\Katalon\\20210406_142923\\execution.properties')
 
 TestCaseMain.beforeStart()
 
-        TestCaseMain.runTestCase('Test Cases/Extra Home Category Test Case/TEST CASE/GDA/TC_CAT_UI_004', new TestCaseBinding('Test Cases/Extra Home Category Test Case/TEST CASE/GDA/TC_CAT_UI_004',[:]), FailureHandling.STOP_ON_FAILURE , false)
+        TestCaseMain.runTestCase('', new TestCaseBinding('',[:]), FailureHandling.STOP_ON_FAILURE , false)
     
